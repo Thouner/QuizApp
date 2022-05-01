cfba969befcf14f111daa08d1e3d248672a67907
Tony Schroeder <tony.schroeder@gmx.de>
2022-05-01 12:55:23 +0200

    complete style

---
+++ backup/js/script.js
@@ -0,0 +1,81 @@
+let questions = [{
+        'question': 'Wie viele Streifen sind auf der US-Flagge?',
+        'answer1': '12',
+        'answer2': '13',
+        'answer3': '14',
+        'answer4': '15',
+        'rightAnswer': 2
+    },
+    {
+        'question': 'Welches Land hat die meisten Inseln in der Welt?',
+        'answer1': 'Norwegen',
+        'answer2': 'Finnland',
+        'answer3': 'Schweden',
+        'answer4': 'Kanada',
+        'rightAnswer': 3
+    },
+    {
+        'question': 'Nenne die meistverkaufte Buchreihe des 21. Jahrhunderts?',
+        'answer1': 'James Bond',
+        'answer2': 'Star Wars',
+        'answer3': 'Fifty Shades of Grey',
+        'answer4': 'Harry Potter',
+        'rightAnswer': 4
+    },
+    {
+        'question': 'Welcher Künstler hat die Decke der Sixtinischen Kapelle in Rom gemalt?',
+        'answer1': 'Michelangelo Buonarroti',
+        'answer2': 'Vincent van Gogh',
+        'answer3': 'Leonardo da Vinci',
+        'answer4': 'Pablo Picasso',
+        'rightAnswer': 1
+    },
+    {
+        'question': 'Wie viele Streifen sind auf der US-Flagge?',
+        'answer1': 'Apollo 11 landete auf dem Mond',
+        'answer2': 'Willy Brandt wird Bundeskanzler',
+        'answer3': 'Richard Nixon wird Präsident der USA',
+        'answer4': 'Apollo 12 landet auf dem Mond',
+        'rightAnswer': 1
+    },
+    {
+        'question': 'Wie viele Tasten hat ein klassisches Klavier?',
+        'answer1': '85',
+        'answer2': '86',
+        'answer3': '87',
+        'answer4': '88',
+        'rightAnswer': 4
+    },
+    {
+        'question': 'Aus welcher Stadt kommen die Beatles?',
+        'answer1': 'Leeds',
+        'answer2': 'London',
+        'answer3': 'Liverpool',
+        'answer4': 'Leicester',
+        'rightAnswer': 3
+    },
+    {
+        'question': 'Wann wurde Netflix gegründet?',
+        'answer1': '1997',
+        'answer2': '2001',
+        'answer3': '2005',
+        'answer4': '2009',
+        'rightAnswer': 1
+    },
+    {
+        'question': 'Welches ist das kleinste Land der Welt?',
+        'answer1': 'San Marino',
+        'answer2': 'Vatikanstadt',
+        'answer3': 'Monaco',
+        'answer4': 'Liechtenstein',
+        'rightAnswer': 2
+    },
+    {
+        'question': 'Wo fanden die ersten modernen Olympischen Spiele statt?',
+        'answer1': 'St. Louis',
+        'answer2': 'Paris',
+        'answer3': 'Athen',
+        'answer4': 'London',
+        'rightAnswer': 3
+    }
+];
